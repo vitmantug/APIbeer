6ba837736178ee78c1ce1036fcfbfa1bc40154b5
vitmantug <vitmantug@gmail.com>
2015-10-18 03:17:29 +0100

    Fixed ElCorteInlges requests;

--- SBPriceCheckerCore/Parsers/ElCorteIngles.cs
+++ SBPriceCheckerCore/Parsers/ElCorteIngles.cs
@@ -17,9 +17,10 @@ namespace SBPriceCheckerCore.Parsers
 {
     public class ElCorteIngles
     {
-        private static string URL_SEARCH = "http://www.elcorteingles.pt/supermercado/sm2/search/resultsProductList.jsp?total_results=19&N=0&Ntt=super+bock&Nty=1&Ntx=mode+matchall&Ntk=SearchDescription&Nr=AND(product.catalogId:010_1,availableCentres:520142)&Np=2&Ntpc=1&Ntpr=1&Nao=0&Nrr=first,field,nterms,exact,numfields,static(varietyDescriptionProperty,ascending)&Nrt=super+bock&Nrk=All&Nrm=matchAll&_requestid=150653";
         private static string URL_PRODUCT_DETAILS = "http://www.elcorteingles.pt/supermercado/sm2/pt_PT/520142/supermarket/bebidas/aguas-sumos-e-cervejas/cervejas/cerveja/{0}";
-        private static string URL_PRODUCT_IMAGE = "http://www.elcorteingles.pt/sgfm/SGFM/dctm/MEDIA02/CONTENIDOS/201507/02/{0}_1__150x150.jpg";
+        
+        private static string URL_LOGIN = "http://www.elcorteingles.pt/supermercado/sm2/login/assignStore.jsp?shopsupermarket&centreId=520142&isPickup=true&shipMth=selfPickup&action=editPymeEditUser&extendData=&fromRegistry=true&externalIntranet=&virtualCenter=&deliveryCentre=&uid=1w4lxL05r9aFCUa31UCO1AVhXOZIhTLG7sMztNPxw-4=";
+        private static string URL_SEARCH = "http://www.elcorteingles.pt/supermercado/sm2/search/resultsProductList.jsp?total_results=19&N=0&Ntt=super+bock&Nty=1&Ntx=mode+matchall&Ntk=SearchDescription&Nr=AND(product.catalogId:010_1,availableCentres:520142)&Np=2&Ntpc=1&Ntpr=1&Nao=0&Nrr=first,field,nterms,exact,numfields,static(varietyDescriptionProperty,ascending)&Nrt=super+bock&Nrk=All&Nrm=matchAll&_requestid=492511";
 
         private Helper Helper = new Helper();
 
@@ -29,8 +30,7 @@ namespace SBPriceCheckerCore.Parsers
 
             try
             {
-                HttpWebRequest webReq = (HttpWebRequest)HttpWebRequest.Create(URL_SEARCH);
-
+                HttpWebRequest webReq = (HttpWebRequest)HttpWebRequest.Create(URL_LOGIN);
                 #region HttpWebRequest headers
                 webReq.CookieContainer = new CookieContainer();
                 webReq.Method = "GET";
@@ -41,25 +41,37 @@ namespace SBPriceCheckerCore.Parsers
                 webReq.Headers.Add("Accept-Language", "pt-PT,pt;q=0.8,en-US;q=0.6,en;q=0.4,es;q=0.2");
                 webReq.Headers.Add("Upgrade-Insecure-Requests", "1");
 
-                webReq.CookieContainer.Add(new Cookie("ASPSESSIONIDACSDDQQD", "IGEOJPPADBPAKKNBIEFPAOON") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("ASPSESSIONIDACTSDTQT", "NBIHGCKDOOGMEBKNFFBKOHBE") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("FECIPTTAM6", "!9i0fscZhCWk5ZRxQqvfntZFj47Q97l0q5ZQhTz8wDlr5VCpmIQ+0l9C9bZoBFi1X1N2AQ0awvQ==") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("JSESSIONID_PROFILE", "00009Jl1j5c6ecoNcGBDvj4tnzP:17mjmcfjh") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("JSESSIONID_SUPERMARKET", "0000inXf-C1XCQHIo3fUKyjzUGW:17mjm7enm") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("PD-H-SESSION-ID", "4_0_CAJZFlTHOdq-LQhUgk0-LJmhjBrgC7SpJ1HAflESvhN5cqrK") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("SITESERVER", "ID=db76594c242bc6f2113ae3de68e1752a") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("__atuvc", "3%7C41") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("_ga", "GA1.2.1595062123.1444947849") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("_gat_UA-42384899-16", "1") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("_hjIncludedInSample", "0") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("cmRS", "t3=1444948206166&pi=CAMPANHAS%3ADESTAQUES") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("cmTPSet", "Y") { Domain = webReq.Host });
-                webReq.CookieContainer.Add(new Cookie("cookiesPolicy", "1") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("ASPSESSIONIDACSDDQQD", "IGEOJPPADBPAKKNBIEFPAOON") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("ASPSESSIONIDACTSDTQT", "NBIHGCKDOOGMEBKNFFBKOHBE") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("FECIPTTAM6", "!9i0fscZhCWk5ZRxQqvfntZFj47Q97l0q5ZQhTz8wDlr5VCpmIQ+0l9C9bZoBFi1X1N2AQ0awvQ==") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("JSESSIONID_PROFILE", "00009Jl1j5c6ecoNcGBDvj4tnzP:17mjmcfjh") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("JSESSIONID_SUPERMARKET", "0000inXf-C1XCQHIo3fUKyjzUGW:17mjm7enm") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("PD-H-SESSION-ID", "4_0_CAJZFlTHOdq-LQhUgk0-LJmhjBrgC7SpJ1HAflESvhN5cqrK") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("SITESERVER", "ID=db76594c242bc6f2113ae3de68e1752a") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("__atuvc", "3%7C41") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("_ga", "GA1.2.1595062123.1444947849") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("_gat_UA-42384899-16", "1") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("_hjIncludedInSample", "0") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("cmRS", "t3=1444948206166&pi=CAMPANHAS%3ADESTAQUES") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("cmTPSet", "Y") { Domain = webReq.Host });
+                //webReq.CookieContainer.Add(new Cookie("cookiesPolicy", "1") { Domain = webReq.Host });
                 #endregion
-
                 WebResponse response = await webReq.GetResponseAsync();
-                Document webpageHtml = NSoupClient.Parse(response.GetResponseStream(), "utf-8");
 
+                HttpWebRequest webReq2 = (HttpWebRequest)HttpWebRequest.Create(URL_SEARCH);
+                #region HttpWebRequest headers
+                webReq2.CookieContainer = webReq.CookieContainer;
+                webReq2.Method = "GET";
+                webReq2.Host = "www.elcorteingles.pt";
+                webReq2.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                webReq2.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36";
+                webReq2.Headers.Add("Accept-Encoding", "gzip, deflate, sdch");
+                webReq2.Headers.Add("Accept-Language", "pt-PT,pt;q=0.8,en-US;q=0.6,en;q=0.4,es;q=0.2");
+                webReq2.Headers.Add("Upgrade-Insecure-Requests", "1");
+                #endregion
+                WebResponse response2 = await webReq2.GetResponseAsync();
+
+                Document webpageHtml = NSoupClient.Parse(response2.GetResponseStream(), "utf-8");
                 Elements beersHtml = webpageHtml.Body.SiblingElements.Select("div.product-tile");
 
                 foreach (Element beerHtml in beersHtml)

02c076b9afebd7f380abd43c0c2ace7557881f13
Vitor Gonçalves <vitmantug@gmail.com>
2015-10-20 20:18:56 +0100

    Implemented a Timer to cache the prices; Implemented ConfigureAwait;

--- SBPriceCheckerCore/Helpers/Helper.cs
+++ SBPriceCheckerCore/Helpers/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using SBPriceCheckerCore.Models;
+using SBPriceCheckerCore.Parsers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -37,21 +38,21 @@ namespace SBPriceCheckerCore.Helpers
         {
             DateTime date = DateTime.Now;
 
-            string dateString = String.Format("{0:dd-MM-yyyy}", date);
+            string dateString = String.Format("{0:HH_dd-MM-yyyy}", date);
             string fileName = dateString + "_" + store + ".prices";
 
             var filePath = Path.Combine(path_, fileName);
 
             using (var stream = File.OpenWrite(filePath))
             using (var writer = new StreamWriter(stream))
-                await writer.WriteAsync(JsonConvert.SerializeObject(beersList));
+                await writer.WriteAsync(JsonConvert.SerializeObject(beersList)).ConfigureAwait(false);
         }
 
         public bool PricesInCache(string store)
         {
             DateTime date = DateTime.Now;
 
-            string dateString = String.Format("{0:dd-MM-yyyy}", date);
+            string dateString = String.Format("{0:HH_dd-MM-yyyy}", date);
             string fileName = dateString + "_" + store + ".prices";
 
             var filePath = Path.Combine(path_, fileName);
@@ -68,14 +69,31 @@ namespace SBPriceCheckerCore.Helpers
         {
             DateTime date = DateTime.Now;
 
-            string dateString = String.Format("{0:dd-MM-yyyy}", date);
+            string dateString = String.Format("{0:HH_dd-MM-yyyy}", date);
             string fileName = dateString + "_" + store + ".prices";
 
             var filePath = Path.Combine(path_, fileName);
 
             using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (var reader = new StreamReader(stream))
-                return await reader.ReadToEndAsync();
+                return await reader.ReadToEndAsync().ConfigureAwait(false);
+        }
+
+        public async Task StorePricesInCache()
+        {
+            List<Beer> _allBeers = new List<Beer>();
+
+            IEnumerable<Beer> beersContinente = await new Continente().GetBeers();
+            _allBeers.AddRange(beersContinente);
+
+            IEnumerable<Beer> beersJumbo = await new Jumbo().GetBeers();
+            _allBeers.AddRange(beersJumbo);
+
+            IEnumerable<Beer> beersElCorteIngles = await new ElCorteIngles().GetBeers();
+            _allBeers.AddRange(beersElCorteIngles);
+
+            if (_allBeers.Any())
+                await InsertBeersRecordAsync(_allBeers, "APIBeer").ConfigureAwait(false);
         }
     }
 }
--- SBPriceCheckerOwinAPI/Program.cs
+++ SBPriceCheckerOwinAPI/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Microsoft.Owin.Hosting;
 using System.Threading;
+using System.Timers;
+using SBPriceCheckerCore.Helpers;
 
 namespace SBPriceCheckerOwinAPI
 {
@@ -19,6 +21,14 @@ namespace SBPriceCheckerOwinAPI
             Console.WriteLine("Starting web Server...");
             WebApp.Start<Startup>(baseUri);
 
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += (s, e) => OnTimedEvent(s, e).SwallowException();
+            aTimer.Interval = 3600000; //1h
+            aTimer.Enabled = true;
+
+            Helper Helper = new Helper();
+            Helper.StorePricesInCache().ConfigureAwait(false);
+
             Console.WriteLine("Server running at {0} - press Enter to quit. ", baseUri);
             //Console.ReadLine();
 
@@ -26,6 +36,20 @@ namespace SBPriceCheckerOwinAPI
             {
                 Thread.Sleep(50);
             }
+        }
+
+        private static async Task OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Helper Helper = new Helper();
+            await Helper.StorePricesInCache().ConfigureAwait(false);
+        }
+    }
+
+    public static class TaskExtensions
+    {
+        public static void SwallowException(this Task task)
+        {
+            task.ContinueWith(_ => { return; });
         }
     }
 }
